4e5aeb7e290a8a01e2548a7c962383f357bdc7c6
Immanuel Dsouza <dsouzad681@gmail.com>
2024-02-07 22:43:01 +0530

    tutorial-2&3

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,16 +1,29 @@
 package controllers
 
 import (
+	"context"
+	"golang-org/x/crypto/bcrypt"
+	"log"
+	"net/http"
+	"strrconv"
+	"time"
+
+	"github.com/Ivan2001otp/Authentication-with-GO/helpers"
+	"github.com/Ivan2001otp/Authentication-with-GO/models"
+	"github.com/gin-gonic/gin"
 	"github.com/Ivan2001otp/Authentication-with-GO/database"
 
 	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/mongo"
-)
+);
 
-var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
-var validate = validator.New()
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client,"user");
+var validate = validator.New();
+
 
 func HashPassword()
+
 
 func SignUp()
 
--- helpers/authHelper.go
+++ helpers/authHelper.go
@@ -1,1 +1,33 @@
 package helpers
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CheckUserType(c *gin.Context, role string) (err error) {
+	userType := c.GetString("user_type")
+	err = nil
+
+	if userType != role {
+		err = errors.New("Unauthorized to access this resources.")
+		return err
+	}
+
+	return err
+}
+
+func MatchUserTypetoUid(c *gin.Context, userId string) (err error) {
+	userType := c.GetString("user_type")
+	uid := c.GetString("uid")
+	err = nil
+
+	if userType == "USER" && uid != userId {
+		err = errors.New("Unauthorized to access this resource.")
+		return err
+	}
+
+	err = CheckUserType(c, userType)
+	return err
+}
